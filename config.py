a535c200b8e1f5f8ad9e04aef4aa2edc69e5cdd4
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2020-12-16 12:23:52 +0300

    База есть. Фронт работает. Контейнер еще не собирал

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
 
 PG_USER = 'blog'
-PG_PASSWORD = os.environ.get('POSTGRES_PASSWORD', '')
-PG_HOST = os.environ.get('PG_HOST', 'database')
+PG_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+
+# PG_HOST = os.environ.get('PG_HOST')     # Dockerbase
+PG_HOST = 'localhost'
 PG_PORT = '5432'
 PG_DB = 'blog_db'
 
 
-SQLALCHEMY_DATABASE_URI=f'postgresql+psycopg2://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}'
+SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}'
